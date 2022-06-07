00b4c79fae8f57dfa072df42d3f36db7c7504d61
Remi Planade <remi.planade@gmail.com>
2022-06-07 17:07:32 +0200

    graphe

---
+++ test/TestGraphe.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestGraphe {
+
+    @Test
+    public void test_Graphe() throws IOException{
+        //preparation des données
+        GrapheListe graphe = new GrapheListe();
+        graphe.ajouterArc("A","B",12);
+        graphe.ajouterArc("A","D",87);
+        graphe.ajouterArc("B","E",11);
+        graphe.ajouterArc("C","A",19);
+        graphe.ajouterArc("D","C",10);
+        graphe.ajouterArc("D","B",23);
+        graphe.ajouterArc("E","D",43);
+
+        //verification
+        assertEquals(5,graphe.getEnsNoeuds().size());
+        assertEquals(5,graphe.getEnsNom().size());
+
+        int nbArc=0;
+        for(int i=0;i<graphe.getEnsNoeuds().size();i++){
+            nbArc+=graphe.getEnsNoeuds().get(i).getAdj().size();
+        }
+        assertEquals(7,nbArc);
+    }
+}
