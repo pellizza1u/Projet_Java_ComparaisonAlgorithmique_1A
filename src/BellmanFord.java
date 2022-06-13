f00acc290d3f4212507523019fea2592ff7d296b
Remi Planade <remi.planade@gmail.com>
2022-06-13 14:55:10 +0200

    BellmanFord algorithm

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -9,13 +9,19 @@ public class BellmanFord {
             v.setParent(i,null);
         }
         v.setValeur(depart,0);
-        for(int k=1; k<g.listeNoeuds().size()-1;k++) {
-            for (int l = 0; l < g.getEnsNoeuds().get(k).getListArc().size(); l++){
-                if(v.getValeur(g.getEnsNoeuds().get(k).getListArc().get(l).getDest())> (v.getValeur(g.suivants(depart).get(l).getDest()) + g.getEnsNoeuds().get(k).getListArc().get(l).getCout())){
-                    v.setValeur(g.getEnsNoeuds().get(k).getListArc().get(l).getDest(),(v.getValeur(g.suivants(depart).get(l).getDest()) + g.getEnsNoeuds().get(k).getListArc().get(l).getCout()));
-                    v.setParent(g.getEnsNoeuds().get(k).getListArc().get(l).getDest(),);
+        boolean stop=false;
+        while(!stop) {
+            stop=true;
+            for (int k = 0; k < g.listeNoeuds().size(); k++) {
+                for (int l = 0; l < g.suivants(g.listeNoeuds().get(k)).size(); l++) {
+                    if (v.getValeur(g.suivants(g.listeNoeuds().get(k)).get(l).getDest()) > v.getValeur(g.listeNoeuds().get(k)) + g.suivants(g.listeNoeuds().get(k)).get(l).getCout()) {
+                        v.setValeur(g.suivants(g.listeNoeuds().get(k)).get(l).getDest(), v.getValeur(g.listeNoeuds().get(k)) + g.suivants(g.listeNoeuds().get(k)).get(l).getCout());
+                        v.setParent(g.suivants(g.listeNoeuds().get(k)).get(l).getDest(), g.listeNoeuds().get(k));
+                        stop=false;
+                    }
                 }
             }
         }
+        return v;
     }
 }
